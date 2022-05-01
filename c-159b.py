ee32dc80f1e6c0c41862e4df20f27fc3389b5fef
ashtonzhang904 <81831416+ashtonzhang904@users.noreply.github.com>
2022-04-30 21:35:14 -0400

    Add files via upload

---
+++ c-159b.py
@@ -0,0 +1,17 @@
+"""dictionary = {"fruit":"Mango",
+              "colour":"pink",
+              "bird": "sparrow"}
+try:
+    print(dictionary["animal"])
+except(KeyError):
+    print("Key animal is not in the dictionary.")"""
+    
+    
+mylist = [0,1,2,3]
+try:
+    print(mylist[5])
+except IndexError:
+    print("Index out of range")
+
+
+
